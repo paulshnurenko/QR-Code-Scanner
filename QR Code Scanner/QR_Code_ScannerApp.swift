cce4d3421be45024dfd885a38c2c234cedee9890
paulshnurenko <paulsnurenko@gmail.com>
2024-03-03 15:20:58 +0100

    Initial Commit

---
+++ QR Code Scanner/QR_Code_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QR_Code_ScannerApp.swift
+//  QR Code Scanner
+//
+//  Created by Pavlo Shnurenko on 03.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct QR_Code_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
