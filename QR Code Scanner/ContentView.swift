11145aba740acc216b43a2703f6ed88a7fa40024
paulshnurenko <paulsnurenko@gmail.com>
2024-03-03 16:01:34 +0100

    Code Update

--- QR Code Scanner/ContentView.swift
+++ QR Code Scanner/ContentView.swift
@@ -6,16 +6,38 @@
 //
 
 import SwiftUI
+import CodeScanner
 
 struct ContentView: View {
+    
+    @State var isPresentingScanner = false
+    @State var scannedCode: String = "Scan a QR code to get started."
+    
+    var scannerSheet: some View {
+        CodeScannerView(
+            codeTypes: [.qr],
+            completion: { result in
+                if case let .success(code) = result {
+                    self.scannedCode = code.string
+                    self.isPresentingScanner = false
+                }
+                
+            }
+        )
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 10) {
+            Text(scannedCode)
+            
+            Button("Scan a QR Code") {
+                self.isPresentingScanner = true
+            }
+            
+            .sheet(isPresented: $isPresentingScanner) {
+                self.scannerSheet
+            }
         }
-        .padding()
     }
 }
 
